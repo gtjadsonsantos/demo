e94fabe21ef9b01ae6465c718fee08fa86cedbf8
Jadson Santos <jadson44.santos@gmail.com>
2021-08-04 22:02:15 -0300

    update

--- src/main/java/core/controllers/ClienteController.java
+++ src/main/java/core/controllers/ClienteController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,27 @@ import core.models.Cliente;
 import core.repositorys.ClienteRespository;
 
 @RestController(value = "clientes")
+@RequestMapping("/clientes")
 public class ClienteController {
       
   @Autowired
   private ClienteRespository repo;
 
-  @RequestMapping("/")
-  public ResponseEntity<List<Cliente>> home() {
-    
-    
+  @GetMapping("/todos")
+  public ResponseEntity<List<Cliente>> pesquisarTodo() {
+     
     return new ResponseEntity<>(repo.findAll(),HttpStatus.OK);
+  }
+
+  @GetMapping("{id}")
+  public ResponseEntity<Cliente> pesquisarPorId(
+    @PathVariable Long id
+  ) {
+     
+    try {
+      return new ResponseEntity<>(repo.findById(id).get(),HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(repo.findById(id).get(),HttpStatus.OK);
+    }
   }
 }
--- src/main/java/core/repositorys/ClienteRespository.java
+++ src/main/java/core/repositorys/ClienteRespository.java
@@ -1,9 +1,12 @@
 package core.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import core.models.Cliente;
 
+@Repository
 public interface ClienteRespository extends JpaRepository<Cliente,Long> {
-    
+
+
 }
